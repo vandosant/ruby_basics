a053dcc82dcf1e1046ed9f38f507025b97f9f4a7
vandosant <sjskender@gmail.com>
2014-03-08 15:38:25 -0700

    Converts double digit number to roman numeral

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -3,20 +3,28 @@
 # Get user input
 puts "Type an integer:"
 x = gets().to_i
-
+roman = ""
+place = 0
 # Convert integer to roman numeral
+if x / 10 > 1
+  (x/10).times do
+    roman += "X"
+    x -= 10
+    place += 1
+  end
+end
 if x > 8
-  roman = "X"
+  roman += "X"
   x -= 10
 end
 if x > 4
-  roman = "V"
+  roman += "V"
   x -= 5
 end
 if x >= 1
   x.times do roman += "I" end
 end
 if x < 0
-  x.abs.times do roman.insert(0, "I") end
+  x.abs.times do roman.insert(place, "I") end
 end
 puts roman
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -18,7 +18,6 @@ describe "RomanNumerals" do
   end
 
   it "converts a double digit number to traditional roman numeral" do
-    pending
     run_script("roman_numerals.rb")
     type("39")
 
